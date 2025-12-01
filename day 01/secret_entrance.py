a1875ad9e878e3a3cdab4d3a81dbcc0f085bef11
Sankari Karthik <sankarikarthik03#gmail.com>
2025-12-01 22:14:29 +0530

    Terribly inefficient but aesthetically pleasing day 1 part b

--- day 01/secret_entrance.py
+++ day 01/secret_entrance.py
@@ -1,7 +1,12 @@
+from functools import reduce 
+
 def get_puzzle_input(directory):
     with open(directory) as f:
         lines = f.read().split()
     return list(map(lambda x: (x[0], int(x[1:])), lines))
+
+def get_expanded_rotation(rotation):
+    return [(rotation[0], 1)] * rotation[1]
 
 def get_position(current_position, rotation):
     match rotation[0]:
@@ -20,5 +25,7 @@ def get_position_frequency(target_position, current_position, rotations):
     # return sum(map(lambda x, y: get_position(x, y) == target_position, rotations))    
 
 rotations = get_puzzle_input(r"./puzzle_input.txt")
-
 print(get_position_frequency(0, 50, rotations))
+
+expanded_rotations = reduce(lambda x, y: x + y, map(get_expanded_rotation, rotations))
+print(get_position_frequency(0, 50, expanded_rotations))
