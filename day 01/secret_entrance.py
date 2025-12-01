97b559bba431a98de6e50874815f99836db1a95b
Sankari Karthik <sankarikarthik03#gmail.com>
2025-12-01 21:53:35 +0530

    Solve day 1 part a

---
+++ day 01/secret_entrance.py
@@ -0,0 +1,24 @@
+def get_puzzle_input(directory):
+    with open(directory) as f:
+        lines = f.read().split()
+    return list(map(lambda x: (x[0], int(x[1:])), lines))
+
+def get_position(current_position, rotation):
+    match rotation[0]:
+        case "L":
+            return (current_position - rotation[1]) % 100
+        case "R":
+            return (current_position + rotation[1]) % 100
+
+def get_position_frequency(target_position, current_position, rotations):
+    frequency = 0
+    for rotation in rotations:
+        current_position = get_position(current_position, rotation)
+        if current_position == target_position:
+            frequency += 1
+    return frequency
+    # return sum(map(lambda x, y: get_position(x, y) == target_position, rotations))    
+
+rotations = get_puzzle_input(r"./puzzle_input.txt")
+
+print(get_position_frequency(0, 50, rotations))
