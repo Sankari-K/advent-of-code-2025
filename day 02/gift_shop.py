d361ab6ec611bba8a58a78e5daa691a1b78a0e26
Sankari Karthik <sankarikarthik03#gmail.com>
2025-12-02 21:15:48 +0530

    Add day 2 part b solution

--- day 02/gift_shop.py
+++ day 02/gift_shop.py
@@ -7,11 +7,23 @@ def get_puzzle_input(directory):
 def get_products(product_ranges):
     return reduce(lambda x, y: x + y, list(map(lambda x: list(range(x[0], x[1] + 1)), product_ranges)))
 
-def sum_invalid_products(products):
-    return sum(map(lambda x: get_invalid(str(x)), products))
+def get_factors(number):
+    return list(filter(lambda x: number % x == 0, range(2, number + 1)))
 
-def get_invalid(product):
-    return int(product) if product[:len(str(product)) // 2] == product[len(str(product)) // 2:] else 0
+def sum_invalid_products(products, only_repeated_twice=False):
+    return sum(map(lambda x: get_invalid(str(x), [2] if only_repeated_twice else get_factors(len(str(x)))), products))
 
-product_ranges = get_puzzle_input(r"./puzzle_input.txt")
-print(sum_invalid_products(get_products(product_ranges)))
+def get_invalid(product, factors):
+    for factor in factors:
+        first_part = product[:len(product) // factor]
+        for multiplier in range(1, factor):
+            if product[multiplier * len(product) // factor: (multiplier + 1) * len(product) // factor] != first_part:
+                break
+        else:
+            return int(product)
+    return 0 
+
+products = get_products(get_puzzle_input(r"./puzzle_input.txt"))
+
+print(sum_invalid_products(products, only_repeated_twice=True)) # part a
+print(sum_invalid_products(products)) # part b
