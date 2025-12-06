4d311963e372c7052dffcb3173c567000fd7ad31
Sankari Karthik <sankarikarthik03#gmail.com>
2025-12-06 18:15:19 +0530

    Add day 4 part a soln

---
+++ day 04/printing_department.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+DIRECTIONS = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
+
+def get_puzzle_input(directory):
+    with open(directory) as f:
+        puzzle_input = f.read().split()
+        grid = defaultdict(str)
+        for x, row in enumerate(puzzle_input):
+            for y, item in enumerate(row):
+                grid[x, y] = item
+        return len(puzzle_input), len(puzzle_input[0]), grid
+
+def check_accessibility(x, y, grid):
+    return sum(map(lambda d: 1 if grid[x + d[0], y + d[1]] == "@" else 0, DIRECTIONS))
+    
+def get_accessible_rolls(grid):
+    accessible_rolls = 0
+    for x in range(X_LIMIT):
+        for y in range(Y_LIMIT):
+            if grid[x, y] == "@" and check_accessibility(x, y, grid) < 4:
+                accessible_rolls += 1
+    return accessible_rolls
+
+X_LIMIT, Y_LIMIT, grid = get_puzzle_input(r"./puzzle_input.txt")
+print(get_accessible_rolls(grid))
